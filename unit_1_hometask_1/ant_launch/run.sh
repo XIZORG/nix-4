ef789ba3ea51a546f05c4ed9c9e156e165d60969
xizorg <leo.jek7@gmail.com>
2021-03-04 15:21:06 +0200

    added ant installation

--- unit_1_hometask_1/ant_launch/run.sh
+++ unit_1_hometask_1/ant_launch/run.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+export PLATFORM_HOME
+PLATFORM_HOME=$(pwd)
+export ANT_OPTS="-Xmx2g -Dfile.encoding=UTF-8"
+export ANT_HOME="$PLATFORM_HOME/apache-ant"
+chmod +x "$ANT_HOME/bin/ant"
+chmod +x "$PLATFORM_HOME/license.sh"
+case "$PATH" in
+    *$ANT_HOME/bin:*) ;;
+    *) export PATH=$ANT_HOME/bin:$PATH ;;
+esac
+
+ant -version
+echo "ant home: ${ANT_HOME}"
+echo "ant opts: ${ANT_OPTS}"
+
 ant compile
 ant jar
 ant run
